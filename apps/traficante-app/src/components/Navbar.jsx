0263655b03df5e3a8d387df1535a381f9aa76687
ricardomaldonadomoreno <135377871+ricardomaldonadomoreno@users.noreply.github.com>
2026-06-09 15:37:35 -0400

    Fix link to user profile in Navbar component

--- apps/traficante-app/src/components/Navbar.jsx
+++ apps/traficante-app/src/components/Navbar.jsx
@@ -37,7 +37,7 @@ export default function TraficanteNavbar({ user, profile }) {
 
           {user ? (
             <>
-              <Link to="/traficante/mi-cuenta" className="navbar-avatar-btn">
+              <Link to="/mi-perfil" className="navbar-avatar-btn">
                 {profile?.avatar_url
                   ? <img src={profile.avatar_url} alt="perfil" className="navbar-avatar-img" />
                   : <div className="navbar-avatar-placeholder">
